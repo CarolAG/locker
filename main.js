f095172ac31cea0a26f86d14d953714e60fb7c8e
carol Gonzalez <carol.argentina.gonzalez@gmail.com>
2016-07-07 17:32:19 -0700

    added code notes in js file and created a dropEffect so that user gets feedback when dragging and dropping

--- main.js
+++ main.js
@@ -7,17 +7,24 @@ if (window.File && window.FileList && window.FileReader) {
 function allowDrop (ev){
   ev.preventDefault();
   ev.stopPropagation();
-
+	ev.dataTransfer.dropEffect = 'copy'
+	// dataTransfer.dropEffect can only be given one of 4 string values: link,
+	//copy, move or none. This is feedback given to the user
+	//https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/dropEffect
 }
 
 function dropHandler (ev){
   ev.preventDefault();
   ev.stopPropagation();
 
-  var files = ev.dataTransfer.files; //FileList object.
+  var files = ev.dataTransfer.files; //FileList object thay contains a list of
+  //all files available on data Transfer
 
 }
 
 var dropZone = document.getElementById('drop_zone');
   dropZone.addEventListener('dragover', allowDrop, false);
   dropZone.addEventListener('drop', dropHandler, false);
+//addEventListener takes in 3 arguments:
+// document.addEventListener(event, function, useCapture)
+//events reference https://developer.mozilla.org/en-US/docs/Web/Events
