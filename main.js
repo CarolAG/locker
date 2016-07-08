947ddf17daee30000fd30c967e6d6181c029247e
carol Gonzalez <carol.argentina.gonzalez@gmail.com>
2016-07-07 18:08:48 -0700

    completed function to handle a drag over the dropzone and to handle the dropping into drop zone

--- main.js
+++ main.js
@@ -19,7 +19,19 @@ function dropHandler (ev){
 
   var files = ev.dataTransfer.files; //FileList object thay contains a list of
   //all files available on data Transfer
+	console.log('files', files);
 
+	// files is a FileList of File objects. List some properties.
+	var output = [];
+	for (car i = 0, f; f = files[i]; i++) {
+		//creating an output string for user to see that gives some info of file
+		//they've dropped
+		output.push('<li><strong>', escape(f.name),'</strong>', f.type || 'n/a', ') - ',
+		f.size, 'bytes, last modified: ', f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString(): 'n/a',
+	'</li>');
+	}
+	//escape deprecated
+	document.getElementById('list').innerHTML = output.join('') + '</ul>';
 }
 
 var dropZone = document.getElementById('drop_zone');
